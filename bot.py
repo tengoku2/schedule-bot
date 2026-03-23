b851563e2681251eb764af2cee020ba2ac6a057a
tengoku2 <lk2g37@gmail.com>
2026-03-23 16:02:02 +0900

    add delete v7

--- bot.py
+++ bot.py
@@ -179,7 +179,7 @@ def load_tasks():
 # -----------------------
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
-tree = app_commands.CommandTree(bot)
+tree = bot.tree
 
 # -----------------------
 # DB INSERT
